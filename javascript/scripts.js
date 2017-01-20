a27135b8dbbc26d2d29cfc0484c03993d08ce086
andrew.nelsonrd@gmail.com <andrewnelson@Andrews-MacBook-Pro.local>
2017-01-20 15:35:55 -0800

    Added new items to my list, and a scroll plugin

--- javascript/scripts.js
+++ javascript/scripts.js
@@ -1,7 +1,24 @@
 $(function(){
-    $('.scrollButtons').on('click','li',function(event){
-        $(this).toggleClass('lilButtonPhase2');
-        // $(this).removeClass('lilButtonPhase1');
-        console.log(event);
+    
+    $('.main-carousel').flickity({
+        // options
+        cellAlign: 'left',
+        contain: true
+});
+
+    $('.buttonwrap').on('click', 'button', function(){
+        var textValue = $('subscribeInput').val();
+        function isEmail(email) {
+            var regex = /^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+            return regex.test(email);
+        }
+
+        if(isEmail(textValue)){
+            alert('Thanks For Subscribing!');
+        } else {
+            alert('Please Enter a Valid Email!');
+        }
+        
     });
+
 });
