7af634127c69f5bbda720fd17e644b31ac611d6e
andrew.nelsonrd@gmail.com <andrewnelson@Andrews-MacBook-Pro.local>
2017-01-22 18:48:21 -0800

    Changed font-size from px to rem, fixed smooth scroll

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
 # Project 1
 
-Used CSS and HTML to create a homepage
-for Aloha Apparel.
+Used CSS, HTML, and JavaScript to create a homepage
+for Aloha Apparel. In the process I expanded on my knowledge of html and css, while 
+learning the library Jquery, which was completely knew to me.
+
+Known Bugs: Justify-Content: flex-end; not working correctly on Safari
--- javascript/scripts.js
+++ javascript/scripts.js
@@ -1,14 +1,36 @@
+
+
 $(function(){
+    
+  $('a[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
+  });    
     
     $('.main-carousel').flickity({
         // options
         cellAlign: 'left',
         contain: true, 
         preNextButtons: false
-});
+    });
 
-    $('.buttonwrap').on('click', 'button', function(){
-        var textValue = $('subscribeInput').val();
+    $('.buttonWrap').on('click', '.subscribeInput', function(){
+        $(this).val('');
+    });
+
+    $('.buttonWrap').on('click', 'button', function(){
+        console.log(this)
+        var textValue = $('.subscribeInput').val();
+        $('.subscriberInput').html().css('color', 'black');
+
         function isEmail(email) {
             var regex = /^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/;
             return regex.test(email);
